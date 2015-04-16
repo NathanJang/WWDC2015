efa82cd47ac914d9f37c2926175ca671a238804a
Jonathan Chan <ycjc@outlook.com>
2015-04-16 23:56:54 +0800

    Separate header file for font names

--- WWDC2015/IntroViewController.swift
+++ WWDC2015/IntroViewController.swift
@@ -6,9 +6,6 @@
 //  Copyright (c) 2015 Jonathan Chan. All rights reserved.
 //
 
-let helveticaNeueUltraLightFontName = "HelveticaNeue-UltraLight"
-let helveticaNeueLightFontName = "HelveticaNeue-Light"
-
 import UIKit
 
 class IntroViewController: UIViewController {
