f019e38d1f6e60d377fe2b0bd0447f6f50e33a1c
Jonathan Chan <ycjc@outlook.com>
2015-04-26 16:20:15 +0800

    Add acknowledgments and finish about view

--- WWDC2015/AboutViewController.swift
+++ WWDC2015/AboutViewController.swift
@@ -10,26 +10,50 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    let margin: CGFloat = 10
+
+    var acknowledgmentsLabel = {() -> UILabel in
+        var acknowledgmentsLabel = UILabel()
+        let path = NSBundle.mainBundle().pathForResource("Acknowledgments", ofType: "txt") as String!
+        let text = NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+        acknowledgmentsLabel.text = text as? String
+        acknowledgmentsLabel.numberOfLines = 0
+        acknowledgmentsLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        return acknowledgmentsLabel
+    }()
+
+    var attributionsLabel = {() -> UILabel in
+        var attributionsLabel = UILabel()
+        let path = NSBundle.mainBundle().pathForResource("Attributions", ofType: "txt") as String!
+        let text = NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+        attributionsLabel.text = text as? String
+        attributionsLabel.numberOfLines = 0
+        attributionsLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        return attributionsLabel
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+
+        self.acknowledgmentsLabel.frame.size.width = self.view.frame.width - 2 * self.margin
+        self.acknowledgmentsLabel.sizeToFit()
+        self.acknowledgmentsLabel.frame.origin = CGPoint(x: self.margin, y: self.margin)
+        self.scrollView.addSubview(self.acknowledgmentsLabel)
+
+        self.attributionsLabel.frame.size.width = self.view.frame.width - 2 * self.margin
+        self.attributionsLabel.sizeToFit()
+        self.attributionsLabel.frame.origin = CGPoint(x: self.margin, y: self.acknowledgmentsLabel.frame.size.height + self.margin)
+        self.scrollView.addSubview(self.attributionsLabel)
+
+        self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.acknowledgmentsLabel.frame.height + self.attributionsLabel.frame.height + self.margin)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ WWDC2015/README.md
@@ -0,0 +1,5 @@
+# Jonathan Chan: WWDC2015
+
+Please use the `.xcworkspace` file to build and run. `:)`
+This app acknowledges and attributes helpful people in `Acknowledgments.txt` and `Attributions.txt`.
+To maintain consistency, American English is used throughout the project (with one exception, if you can find it), although I prefer to use British English.
